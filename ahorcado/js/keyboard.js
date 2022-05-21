2528edddc0f49d57281cfd1874302f66704e5646
acamus79 <adrianedgardocamus@gmail.com>
2022-05-20 23:44:25 -0300

    Update keyboard.js

--- ahorcado/js/keyboard.js
+++ ahorcado/js/keyboard.js
@@ -1,40 +1,30 @@
 //const abecedario = "1234567890abcdefghijklmnñopqrstuvwxyzç., ";
 const querty = "QWERTYUIOPASDFGHJKLÑZXCVBNM";
-const idLetras=document.getElementById("letras");
- 
+const idLetras = document.getElementById("letras");
 
 // funcion para mostrar las letras, recibe por parametro el abecedario
 const dibujaTeclas = listadoLetras => {
-
     idLetras.innerHTML="";
-
     // añadimos las letras
-
-    listadoLetras.split('').map(el => {
+    listadoLetras.split('').map(elemento => {
         let span=document.createElement("span");
         span.addEventListener("click", teclaPulsada);
-        span.innerText=el;
+        span.innerText = elemento;
         idLetras.appendChild(span);
-
     });
-
 }
 
 // por defecto indicamos que muestre el teclado querty
 dibujaTeclas(querty);
 
-
 // función que recibe la pulsación de la tecla
 // tanto del teclado fisico como del teclado dibujado en la pantalla
-
 function teclaPulsada(e) {
-
     const tecla = this.classList && this.classList.contains("space") ? " " : this.innerText;
-
+    
     if (querty.indexOf(tecla)>=0) {
-
         document.getElementById("texto").value+=tecla;
-
+        tecla.style.color = "indianred";
     }
 
 }
