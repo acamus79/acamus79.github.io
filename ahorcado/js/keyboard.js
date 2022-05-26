43fc039b3a5cc2b7fa86e02898504a065d07605b
acamus79 <adrianedgardocamus@gmail.com>
2022-05-25 23:39:48 -0300

    Update keyboard.js

--- ahorcado/js/keyboard.js
+++ ahorcado/js/keyboard.js
@@ -1,6 +1,8 @@
 //const completo = "1234567890abcdefghijklmnñopqrstuvwxyzç., ";
-const querty = "QWERTYUIOPASDFGHJKLÑZXCVBNM";
+//const querty = "QWERTYUIOPASDFGHJKLÑZXCVBNM";
+const abc = "ABCDEFJHIJKLMNÑOPQRSTUVWXYZ"
 const idTeclas = document.getElementById("teclas");
+let palabraJugando = [];
 
 // funcion para mostrar las letras, recibe por parametro el abecedario
 const dibujaTeclas = listadoLetras => {
@@ -15,16 +17,22 @@ const dibujaTeclas = listadoLetras => {
 }
 
 // por defecto indicamos que muestre el teclado querty
-dibujaTeclas(querty);
+dibujaTeclas(abc);
 
 // función que recibe la pulsación de la tecla
 // tanto del teclado fisico como del teclado dibujado en la pantalla
 function teclaPulsada(e) {
     const tecla = this.classList && this.classList.contains("space") ? " " : this.innerText;
     
-    if (querty.indexOf(tecla)>=0) {
+    if (abc.indexOf(tecla)>=0) {
         document.getElementById("texto").value+=tecla;
+        palabraJugando.push(tecla);
+        console.log(palabraJugando);
         return tecla.value;
     }
+}
 
+// Funcion JavaScript para la conversion a mayusculas
+function mayusculas(e) {
+    e.value = e.value.toUpperCase();
 }
